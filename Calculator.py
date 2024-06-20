89660dadc0eaa1f9eee428ebfec6bd2e5f2ff73c
Kostya Kuzin <hrustyshka34@mail.ru>
2024-06-20 19:02:06 +0400

    Начало проекта
    Заготовочные файлы созданы

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+
+
+
+    def sum(self, a, b):
+        pass
+
+    def subs(self, a, b):
+        pass
+
+    def mul(self, a, b):
+        pass
+
+    def div(self, a, b):
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from Calculator import Calculator
+
+
+if __name__ == "__main__":
+
+    c = Calculator()
+
+    print(c)

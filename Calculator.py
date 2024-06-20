431cce680da85ebb66ca5a9bdbdb02d3d4d5aa23
PsiHkk <01psih002@gmail.com>
2024-06-20 18:19:46 +0300

    Update Calculator.py
    sum and mul

--- Calculator.py
+++ Calculator.py
@@ -3,12 +3,14 @@ class Calculator:
 
 
     def sum(self, a, b):
+        if a > 0 and b > 0: return a+b
         pass
 
     def subs(self, a, b):
         pass
 
     def mul(self, a, b):
+        return a * b
         pass
 
     def div(self, a, b):
